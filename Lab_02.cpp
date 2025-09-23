5764f6c4f36014b9dfdf0e813a812d27ec7bb4b7
bozhenashchurri2025 <bozhena.shchur.ri.2025@lpnu.ua>
2025-09-23 17:34:41 +0300

    Перший коміт

---
+++ Lab_02.cpp
@@ -0,0 +1,29 @@
+// Щур Божена
+// Лабораторна робота No 2.
+// Лінійні програми.
+// Варіант 27
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double a;   // вхідний параметр
+    double z1;  // результат першого виразу
+    double z2;  // результат другого виразу
+
+    cout << "a = ";
+    cin >> a;
+
+    // Обчислення
+    z1 = pow(cos(3.0 * M_PI / 8.0 - a/ 4.0), 2)
+         - pow(cos(11.0 * M_PI / 8.0 + a / 4.0), 2);
+    z2 = (sqrt(2) / 2.0) * sin(a / 2.0);
+
+    // Вивід
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
---
+++ Lab_02.h
@@ -0,0 +1,13 @@
+//
+// Created by Bizha Shchur on 22.09.2025.
+//
+
+#ifndef PR1_LAB_02_H
+#define PR1_LAB_02_H
+
+
+class Lab_02 {
+};
+
+
+#endif //PR1_LAB_02_H
